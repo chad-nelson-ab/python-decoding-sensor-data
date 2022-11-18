6ecad6caaec0cf69a9317b497dff54dcab8a0cad
Chad Nelson <chad.nelson@anheuser-busch.com>
2022-11-18 12:42:37 -0600

    commit

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -2,9 +2,9 @@
 from statistics import mean
 from load_data import load_sensor_data
 from house_info import HouseInfo
-from datetime import datetime, date
-from sensor.humidity_info import HumidityData
-from sensor.particle_count_info import ParticleData
+from datetime import datetime
+from humidity_info import HumidityData
+from particle_count_info import ParticleData
 from temperature_info import TemperatureData
 ##############################
 # Do not remove these two lines
@@ -43,8 +43,7 @@ print("\nHouse Humidity sensor records for area {} = {}".format(test_area, len(r
 print("\tAverage: {} humidity".format(mean(recs)))
 
 recs = humidity_data.get_data_by_date(rec_date=test_date)
-print("House Humidity sensor records for date: {} = {}".format(
-    test_date.strftime("%m/%d/%y"), len(recs)))
+print("House Humidity sensor records for date: {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
 print("\tAverage: {} humidity".format(mean(recs)))
 
 particle_data = ParticleData(data)
@@ -54,4 +53,13 @@ concentrations = particle_data.get_data_concentrations(data=recs)
 print("\tGood Air Quality Recs: {}".format(concentrations["good"]))
 print("\tModerate Air Quality Recs: {}".format(concentrations["moderate"]))
 print("\tBad Air Quality Recs: {}".format(concentrations["bad"]))
+
+recs = particle_data.get_data_by_date(rec_date=test_date)
+print("\nHouse Particle sensor records for date: {} = {}".format(
+    test_date.strftime("%m/%d/%y"), len(recs)))
+concentrations = particle_data.get_data_concentrations(data = recs)
+print("\tGood Air Quality Recs: {}".format(concentrations["good"]))
+print("\tModerate Air Quality Recs: {}".format(concentrations["moderate"]))
+print("\tBad Air Quality Recs: {}".format(concentrations["bad"]))
+
 # Module 5 code here:
