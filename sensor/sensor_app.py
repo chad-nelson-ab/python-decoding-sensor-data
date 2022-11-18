ceec3a5d881017eef940e92cd72f2ade6b493f5b
Chad Nelson <chad.nelson@anheuser-busch.com>
2022-11-18 12:35:05 -0600

    commit

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,7 +1,9 @@
 # Runner script for all modules
+from statistics import mean
 from load_data import load_sensor_data
 from house_info import HouseInfo
 from datetime import datetime, date
+from sensor.humidity_info import HumidityData
 from temperature_info import TemperatureData
 ##############################
 # Do not remove these two lines
@@ -34,5 +36,13 @@ recs = temperature_data.get_data_by_date(rec_date=test_date)
 print("\nHouse Temperature sensor records for date: {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
 print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs)))
 # Module 4 code here:
+humidity_data = HumidityData(data)
+recs = humidity_data.get_data_by_area(rec_area=test_area)
+print("\nHouse Humidity sensor records for area {} = {}".format(test_area, len(recs)))
+print("\tAverage: {} humidity".format(mean(recs)))
 
+recs = humidity_data.get_data_by_date(rec_date=test_date)
+print("House Humidity sensor records for date: {} = {}".format(
+    test_date.strftime("%m/%d/%y"), len(recs)))
+print("\tAverage: {} humidity".format(mean(recs)))
 # Module 5 code here:
